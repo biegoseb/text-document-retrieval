2477fa3bcd4ce5d23d6d5931aaf776eeb497a254
Diego <diego@archlinux.localdomain>
2020-11-28 17:27:44 -0500

    client created

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Browser/>
+  </div>
+</template>
+
+<script>
+import Browser from "./components/Browser.vue";
+
+export default {
+  name: "App",
+  components: {
+    Browser,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Browser.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-group mb-3">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="button-addon2"
+    />
+    <div class="input-group-append">
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+        @click="search_query()"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Browser',
+  data() {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    async search_query() {
+      const body = {
+        query: this.query
+      }
+      axios.post('http://127.0.0.1:8081/query', body).then((response) => {
+        console.log(response);
+      })
+    }
+  }
+};
+</script>
+
+<style></style>
